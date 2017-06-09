1ab09d390d373845fb5b08529b858b8427bfd078
Ryan Mack <ryanjmack21@gmail.com>
2017-06-09 19:56:25 -0400

    Cleaned up folder

--- week1/happy.py
+++
@@ -1,4 +0,0 @@
-"""Write a piece of Python code that prints out the string 'hello world' if the value of an integer variable, happy, is strictly greater than 2."""
-
-if happy > 2:
-    print("hello world")
